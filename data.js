e17f05816425c38bde2beead446c8c4c72f85a11
Durgesh-9 <durgeshlakhera980@gmail.com>
2024-08-05 14:28:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'Kahani Suno',
+        artist: 'Kaifi Khalil',
+        path: 'song/1.mp3',
+        cover: 'images/1.jpg'
+    },
+    {
+        name :  'Chaleya',
+        artist : 'Arijit Singh, Shilpa Rao',
+        path : 'song/2.mp3',
+        cover : 'images/2.jpg'
+    },
+    {
+        name : 'Soulmate',
+        artist : 'Badshah, Arijit Singh',
+        path : 'song/3.mp3',
+        cover : 'images/3.jpg'
+    },
+    {
+        name: 'Heeriye',
+        artist : 'Arijit Singh',
+        path : 'song/4.mp3',
+        cover : 'images/4.jpg'
+    }
+];
